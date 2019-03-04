810c7c496632a8003a3f4a8648b168c508993c4d
Khaled <disasterz9955@gmail.com>
2019-03-04 21:49:52 +0300

    Routing nesting

--- intro/src/components/Topics.jsx
+++ intro/src/components/Topics.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { Link, Route } from "react-router-dom";
+
+function Topic({ match }) {
+  return <h3>TOPIC</h3>;
+}
 
 export default function Topics() {
-  return <div>Topics</div>;
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to="/topics/rendering">Rendering With React</Link>
+        </li>
+        <li>
+          <Link to="/topics/components">Components</Link>
+        </li>
+        <li>
+          <Link to="/topics/props-v-state">Props vs State</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route to="/topics/:topicId" component={Topic} />
+      <Route
+        exact
+        to="/topics"
+        render={() => {
+          return <h3>Please select a topic</h3>;
+        }}
+      />
+    </div>
+  );
 }
